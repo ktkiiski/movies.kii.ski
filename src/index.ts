d7a774a2cf1f7c3d234e673330d23fd78ed9bc1e
Kimmo Kiiski <kimmo@kii.ski>
2017-08-09 23:55:41 +0300

    Log defined constants for demo purposes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import './index.scss';
 
 // Write your code here!
+
+import imageUrl = require('./images/broilerplate.png');
+// tslint:disable:no-console
+console.log(`Commit: ${__COMMIT_HASH__}`);
+console.log(`Version: ${__VERSION__}`);
+console.log(`Branch: ${__BRANCH__}`);
+console.log(`Example image URL: ${imageUrl}`);
+// tslint:enable:no-console
